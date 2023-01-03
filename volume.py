33bdc98fef1133b95d62e1e587e53da9c0f7cd48
Pedro Ricardo <115710304+pedroricardo14@users.noreply.github.com>
2023-01-03 16:24:09 -0300

    Create volume.py
    Programa que calcula o volume(m²) de um objeto.

---
+++ volume.py
@@ -0,0 +1,9 @@
+comprimento = float(input("Comprimento? "))
+
+largura = float(input("Largura? "))
+
+altura = float(input("Altura?" ))
+
+volume = comprimento * largura * altura
+
+print(f"O volume é {volume} m².")
